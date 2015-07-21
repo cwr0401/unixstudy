5dd8f72bab1c7feb63230d3952d88fc2aec11433
cwr0401 <cwr0401@gmail.com>
2015-07-21 20:26:52 +0800

    rm out file

--- apue/chapter15/showshm.c
+++ apue/chapter15/showshm.c
@@ -10,5 +10,7 @@ int main(int argc, char *argv[]){
 	if((shmptr = shmat(atoi(argv[1]), 0, SHM_RDONLY)) == (void *)-1)
 		err_sys("shmat error, shmkey:%d", argv[1]);
 	printf("shmptr: %p\n", shmptr);
-	printf("%s", shmptr);
+	for(int i = 0; i < 10; i++){
+		printf("%d", shmptr[i]); 
+	}	
 }
